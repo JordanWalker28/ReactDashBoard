b70f0e7d94e93e6986b617f5d867219cf2ea083b
Jordan Walker <jordanswalker28@gmail.com>
2022-01-06 21:47:10 +0000

    Added Google Fonts

---
+++ cryptodash/src/App/index.js
@@ -0,0 +1,31 @@
+import './App.css';
+import WelcomeMessage from './WelcomeMessage';
+import { Component } from 'react';
+import styled, {css} from 'styled-components';
+
+// const MyButton = styled.div`
+//   color: green;
+
+//   ${props => props.primary && css`
+//     color: palevioletred;
+//   `}
+// `
+
+// const TomatoButton = styled(MyButton)`
+//   color: tomato;
+//   border-color: tomato;
+// `;
+
+class App extends Component {
+  render(){
+    return (
+      <div>
+        <WelcomeMessage/> 
+        {/* <MyButton>Hello</MyButton>
+        <MyButton primary>Hello</MyButton>
+        <TomatoButton>Hello</TomatoButton> */}
+      </div>
+    )
+}}
+
+export default App;

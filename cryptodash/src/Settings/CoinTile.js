0a630ab7c7c6b9c6b65cdcaec47710c3a8cba714
Jordan Walker <jordanswalker28@gmail.com>
2022-01-09 18:38:26 +0000

    Refactored

---
+++ cryptodash/src/Settings/CoinTile.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { AppContext } from '../App/AppProvider';
+import { SelectableTile } from '../Shared/Tile';
+
+export default function ({coinKey}){
+
+    return <AppContext.Consumer>
+        {({coinList}) => {
+            let coin = coinList[coinKey];
+
+            const TileClass = SelectableTile;
+            return <TileClass>
+                {coin.CoinName}
+            </TileClass>
+        }}
+    </AppContext.Consumer>
+}

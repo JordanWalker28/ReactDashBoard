43dc069d1cf88ae5a87d664f85cf889964b95127
Jordan Walker <jordanswalker28@gmail.com>
2022-01-09 22:37:26 +0000

    Updated Layout

--- cryptodash/src/Settings/CoinGrid.js
+++ cryptodash/src/Settings/CoinGrid.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import styled, {css} from 'styled-components';
+import styled from 'styled-components';
 import {AppContext} from "../App/AppProvider";
-import {Coin} from '../Shared/Tile'
 import CoinTile from './CoinTile';
 
 export const CoinGridStyled = styled.div`
     display: grid;
     grid-template-columns: repeat(5, 1fr);
     grid-gap: 15px;
+    margin-top: 45px;
 `
 
 function getcoinsToDisplay(coinList){
     return Object.keys(coinList).slice(0,100);
 }
 
-export default function() {
+export default function CoinGrid() {
     return <AppContext.Consumer>
         {({coinList}) => <CoinGridStyled>
             {getcoinsToDisplay(coinList).map(coinKey =>
                 <CoinTile coinKey ={coinKey}/>
-                )}
+            )}
         </CoinGridStyled>}
     </AppContext.Consumer>
 }
--- cryptodash/src/Settings/CoinTile.js
+++ cryptodash/src/Settings/CoinTile.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { AppContext } from '../App/AppProvider';
 import { SelectableTile } from '../Shared/Tile';
+import CoinHeaderGrid from './CoinHeaderGrid';
+import CoinImage from '../Shared/CoinImage';
 
-export default function ({coinKey}){
-
+export default function CoinTile({coinKey}){
     return <AppContext.Consumer>
         {({coinList}) => {
             let coin = coinList[coinKey];
-
             const TileClass = SelectableTile;
+
             return <TileClass>
-                {coin.CoinName}
+                <CoinHeaderGrid name ={coin.CoinName} symbol = { coin.Symbol}/>
+                <CoinImage coin = { coin}/>
             </TileClass>
         }}
     </AppContext.Consumer>

aee98b178f1cdbd13cde98e84670c8bbef2e6efe
Jordan Walker <jordanswalker28@gmail.com>
2022-01-09 17:42:09 +0000

    Added CoinGrid

--- cryptodash/src/Settings/index.js
+++ cryptodash/src/Settings/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import WelcomeMessage from './WelcomeMessage';
 import ConfirmButton from './ConfirmButton';
 import Page from '../Shared/Page'
+import CoinGrid from './CoinGrid'
 
 export default function Index() {
     return <Page name ="Settings">
         <WelcomeMessage/>
         <ConfirmButton/>
+        <CoinGrid/>
     </Page>
 }

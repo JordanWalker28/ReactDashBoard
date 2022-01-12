8b3ad57188f59d74c107fc60055659bb167f49ad
Jordan Walker <jordanswalker28@gmail.com>
2022-01-12 22:05:03 +0000

    Add Search

--- cryptodash/src/Settings/index.js
+++ cryptodash/src/Settings/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import WelcomeMessage from './WelcomeMessage';
 import ConfirmButton from './ConfirmButton';
-import Page from '../Shared/Page'
-import CoinGrid from './CoinGrid'
+import Page from '../Shared/Page';
+import CoinGrid from './CoinGrid';
+import Search from './Search';
 
 export default function Index() {
     return <Page name ="Settings">
         <WelcomeMessage/>
         <CoinGrid topSection/>
         <ConfirmButton/>
+        <Search/>
         <CoinGrid/>
     </Page>
 }

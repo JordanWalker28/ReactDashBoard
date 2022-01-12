c8fcbd938ec600f267c323bd79e5c7830d094ad0
Jordan Walker <jordanswalker28@gmail.com>
2022-01-12 22:08:42 +0000

    Changes to styles

--- cryptodash/src/Settings/Search.js
+++ cryptodash/src/Settings/Search.js
@@ -5,7 +5,7 @@ import {backgroundColor2, fontSize2} from '../Shared/Styles';
 const SearchGrid = styled.div`
     display: grid;
     grid-template-columns: 200px 1fr;
-    margin-left: 40%;
+    margin-left: 20%;
 `
 
 const SearchInput = styled.input`
@@ -14,6 +14,7 @@ const SearchInput = styled.input`
     border: 1px solid;
     height: 25px;
     place-self: center left;
+    width: 80%
     color: white;
 `
 

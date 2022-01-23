2409930948c2160a1643b7bc03f5c2d4a7f44b83
Jordan Walker <jordanswalker28@gmail.com>
2022-01-23 18:38:56 +0000

    Add graph type dropdown

--- cryptodash/src/Dashboard/PriceChart.js
+++ cryptodash/src/Dashboard/PriceChart.js
@@ -3,12 +3,18 @@ import highchartsConfig from './HighchartsConfig';
 import {Tile} from '../Shared/Tile';
 import {AppContext} from '../App/AppProvider';
 import ReactHighcharts from 'react-highcharts';
+import ChartSelect from './ChartSelect';
 
 export default function PriceChart() {
     return(
         <AppContext.Consumer>
         {({historical}) => 
             <Tile>
+                <ChartSelect>
+                    <option value = "Days">Days</option>
+                    <option value = "Weeks">Weeks</option>
+                    <option value = "Months">Months</option>
+                </ChartSelect>
                 {historical ? 
                     <ReactHighcharts config = {highchartsConfig(historical)} />
                     : <div> Loading historical Data</div>    
